278686a1de69f9e2b1c0a8069fe8204865afc23a
nandinigoli09 <245121737105@mvsrec.edu.in>
2024-03-05 12:11:52 +0530

    gitdemo

---
+++ sample.py
@@ -0,0 +1,47 @@
+<html>
+	<head>
+		<title>registation from</title>
+	</head>
+	<body>
+		<h1>regristation from</h1>
+		<table border="align=10" cellspacing="10" cellspadding="10">
+			<form>
+				<tr>
+					<td>Name<input type="text"><br</td>
+				</tr>
+				<tr>
+					<td>Gender</td>
+					<td><input type="radio">male<br></td>
+					<td><input type="radio">female<br></td>
+					<td><input type="radio">others<br></td>
+				</tr>
+				<tr>
+					<td>email id<input type="email" name=" "><br></td>
+				</tr>
+				<tr>
+					<td>password<input type="password" name=" "><br></td>
+				</tr>
+				<tr>
+					<td>address<input textarea rows="40" cols="40"><textarea></td>
+				</tr>
+				<tr>
+					<td>Qualification<select>
+							<td><select><option>-----select-----</option>
+							<option><Btech/B.E></option>
+							<option><M.E/mtech></option>
+							<option><degree></option>
+							<option><inter></option>
+					</td>
+				</tr>
+				<tr>
+					<td>subject</td>
+					<td><input type="checkbox">moblie application<br></td>
+					<td><input type="checkbox">web development<br></td>
+					<td><input type="checkbox">deveops<br></td>
+				</tr>
+				</table>
+			</form>
+	</body>
+	</htmL>
+
+:
